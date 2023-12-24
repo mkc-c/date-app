19e28e42a09676991ba46559061642bc4860bb78
mkc-c <abdirisak.arr@gmail.com>
2023-12-24 03:39:10 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [steps, setSteps] = useState(1);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  function handleStepDown() {
+    setSteps((currentStep) => currentStep - 1);
+  }
+
+  function handleStepUp() {
+    setSteps((currentStep) => currentStep + 1);
+  }
+
+  function handleCountDown() {
+    setCount((currentCount) => {
+      if (steps === 1) return count - 1;
+      else return currentCount - steps;
+    });
+
+    setDate(new Date(date.setDate(date.getDate() - 1)));
+  }
+
+  function handleCountUp() {
+    setCount((currentCount) => {
+      if (steps === 1) return count + 1;
+      else return currentCount + steps;
+    });
+
+    setDate(new Date(date.setDate(date.getDate() + 1)));
+  }
+
+  function formattedDate() {
+    if (date) return date.toDateString();
+  }
+
+  function renderedDate() {
+    if (date.toDateString() === new Date().toDateString() || date === 0) {
+      return `Today is ${formattedDate()}`;
+    } else {
+      return `${count} days from today is ${formattedDate()}`;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="date-changer">
+      <div className="steps">
+        <button onClick={handleStepDown}>-</button>
+        <span>{steps}</span>
+        <button onClick={handleStepUp}>+</button>
+      </div>
+      <div className="count">
+        <button onClick={handleCountDown}>-</button>
+        <span>{count}</span>
+        <button onClick={handleCountUp}>+</button>
+      </div>
+      <div className="date">{renderedDate()}</div>
     </div>
   );
 }
